e5799f7bad3f62368d14d277b3f33c8ba4fc0576
Prithviraj8 <prithvimurthy8@gmail.com>
2022-01-22 18:36:35 +0530

    add fix

--- apy/services/services.py
+++ apy/services/services.py
@@ -2,11 +2,12 @@ import requests
 
 
 class Services(object):
-    def __init__(self, token):
+    def __init__(self, token, token_ticker):
         self.token = token
+        self.token_ticker = token_ticker
 
     def token_price(self):
-        url = f"https://api.binance.com/api/v3/ticker/price?symbol={self.token}USDT"
+        url = f"https://api.binance.com/api/v3/ticker/price?symbol={self.token}{self.token_ticker}"
         request = requests.get(url)
         data = request.json()
         return data
--- utilities/token_utilities.py
+++ utilities/token_utilities.py
@@ -1,18 +1,19 @@
 from apy.services import Services
+from utilities.eumn_token_symbols import TokenSymbols
 
 
 class TokenUtilities:
-    def get_token_price_in_eth(token_ticker):
+    def get_token_price_in_eth(reward_token_ticker, token_ticker):
 
         try:
-            services = Services(token=token_ticker)
+            services = Services(token=reward_token_ticker, token_ticker=token_ticker)
             data = services.token_price()
             token_in_usdt = float(data["price"])
         except Exception as e:
             token_in_usdt = 1.0
 
         try:
-            services = Services("ETH")
+            services = Services(TokenSymbols.ETH.value, token_ticker=token_ticker)
             data = services.token_price()
             eth_in_usdt = float(data["price"])
         except Exception as e:
